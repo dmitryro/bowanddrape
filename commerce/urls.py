0b0204b79e95112e96baea9d0f233ce2abe59746
root <root@u17749893.onlinehome-server.com>
2016-08-14 18:53:59 -0400

    local urls updated

--- commerce/urls.py
+++ commerce/urls.py
@@ -1,4 +1,4 @@
-"""commerce URL Configuration
+"""clmmerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -23,20 +23,29 @@ from oscarapicheckout.app import application as oscar_api_checkout
 from oscar.app import application
 from django.conf import settings
 from django.conf.urls.static import static
+from oscar_accounts.views import AccountBalanceView
+from paypal.express.dashboard.app import application as paypal_application
 
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/', admin.site.urls),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+    url(r'^dashboard/stripe/', include(accounts_app.urls)),
+    url(r'^giftcard-balance/', AccountBalanceView.as_view()),
+            # Conditions
  #   url(r'^giftcard-balance/', AccountBalanceView.as_view(),
   #      name="account-balance"),
     # urls.py
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r"^payments/", include("pinax.stripe.urls")),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
+
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^dashboard/accounts/', include(accounts_app.urls)),
+    url(r'^checkout/paypal/', include('paypal.express.urls')),
+    url(r'^dashboard/paypal/express/', include(paypal_application.urls)),
+
     url(r'^ocapi/', include(oscar_api_checkout.urls)), # Must be before oscar_api.urls
     url(r'^ocapi/', include(oscar_api.urls)),
     url(r'', include(application.urls)),
